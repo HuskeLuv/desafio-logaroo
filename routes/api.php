b1a7f6f98b5ec107f851888f39bb70c45c3fb069
Wellington <huske.xp@gmail.com>
2024-05-08 20:48:55 -0300

    chore: Nomeando rotas para testes

--- routes/api.php
+++ routes/api.php
@@ -9,20 +9,20 @@ Route::get('/', function (){
 });
 
 Route::prefix('auth')->group(function (){
-  Route::post('register', [AuthController::class, 'register']);
-  Route::post('login', [AuthController::class, 'login']);
+  Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+  Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 });
 
 Route::middleware('auth:api')->group(function () {
   Route::prefix('auth')->group(function () {
-    Route::get('me', [AuthController::class, 'me']);
-    Route::delete('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me'])->name('auth.me');
+    Route::delete('logout', [AuthController::class, 'logout'])->name('auth.logout');
   });
 });
 
 Route::prefix('posts')->middleware('auth:api')->group(function() {
-  Route::get('/', [PostController::class, 'index']);
-  Route::post('/',[PostController::class, 'store']);
-  Route::put('{post}',[PostController::class, 'update']);
-  Route::delete('{post}',[PostController::class, 'destroy']);
+  Route::get('/', [PostController::class, 'index'])->name('posts.index');
+  Route::post('/',[PostController::class, 'store'])->name('posts.store');
+  Route::put('{post}',[PostController::class, 'update'])->name('posts.update');
+  Route::delete('{post}',[PostController::class, 'destroy'])->name('posts.destroy');
 });
