ec4bfe51510ff6d52ffdfe8f7211b15c5323cfd4
Wellington <huske.xp@gmail.com>
2024-05-08 20:49:41 -0300

    test: Atualizando TestCase.php e removendo exemplos

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
-abstract class TestCase extends BaseTestCase
-{
-    //
+abstract class TestCase extends BaseTestCase {
+
+    protected function registerUser(array $userData) {
+        return $this->json('POST', '/api/auth/register', $userData);
+    }
+
+    public function createUser($args = []) {
+        return User::factory()->create($args);
+    }
+
+    public function authUser() {
+        $user = $this->createUser();
+        $token = JWTAuth::fromUser($user);
+        return $token;
+    }
 }
